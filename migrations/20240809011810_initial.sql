cb2a14d39e2d430e5b1b5f91c06c278dd023f8d9
maya <770257551@qq.com>
2024-08-22 15:36:43 +0800

    featuer: add triggers to postgres

--- migrations/20240809011810_initial.sql
+++ migrations/20240809011810_initial.sql
@@ -2,11 +2,29 @@
 -- create user table
 CREATE TABLE IF NOT EXISTS users (
     id BIGSERIAL PRIMARY KEY,
+    ws_id BIGINT NOT NULL,
     fullname VARCHAR(64) NOT NULL,
     email VARCHAR(64) NOT NULL,
     password_hash VARCHAR(97) NOT NULL,
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
+-- workspace for users
+CREATE TABLE IF NOT EXISTS workspace (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(32) NOT NULL UNIQUE,
+    owner_id BIGINT NOT NULL REFERENCES users(id),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+BEGIN;
+-- add super user 0
+INSERT INTO users (id, ws_id, fullname, email, password_hash)
+VALUES (0, 0, 'super user', 'super@none.org', '');
+INSERT INTO workspace(id, name, owner_id)
+VALUES (0, 'none', 0);
+commit;
+-- add foreign key constraint for ws_id for users
+ALTER TABLE users
+ADD CONSTRAINT users_ws_id_fkey FOREIGN KEY (ws_id) REFERENCES workspace(id);
 -- create index for users for email
 CREATE UNIQUE INDEX IF NOT EXISTS email_index ON users(email);
 -- create chat type: single, group, private_channel, public_channel
@@ -19,6 +37,7 @@ CREATE TYPE chat_type AS ENUM (
 -- create chat table
 CREATE TABLE IF NOT EXISTS chats(
     id BIGSERIAL PRIMARY KEY,
+    ws_id BIGINT REFERENCES workspace(id),
     name VARCHAR(64),
     type chat_type NOT NULL,
     -- user id list
